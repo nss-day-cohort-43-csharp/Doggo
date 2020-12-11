c929930c3108c04b662a66ea9edc7ea5fb7091dd
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2020-12-10 19:13:06 -0600

    get all walkers and walker by id

---
+++ Doggo/Controllers/WalkersController.cs
@@ -0,0 +1,100 @@
+﻿using Doggo.Models;
+using Doggo.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doggo.Controllers
+{
+    public class WalkersController : Controller
+    {
+        private IWalkerRepository _walkerRepo;
+
+        public WalkersController(IWalkerRepository walkerRepo)
+        {
+            _walkerRepo = walkerRepo;
+        }
+
+        // GET: WalkersController
+        public ActionResult Index()
+        {
+            List<Walker> walkers = _walkerRepo.GetAllWalkers();
+
+            return View(walkers);
+        }
+
+        // GET: walkers/details/1023
+        public ActionResult Details(int id)
+        {
+            Walker walker = _walkerRepo.GetWalkerById(id);
+
+            return View(walker);
+        }
+
+        // GET: WalkersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: WalkersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WalkersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: WalkersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: WalkersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: WalkersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Doggo/Repositories/IWalkerRepository.cs
@@ -0,0 +1,11 @@
+﻿using Doggo.Models;
+using System.Collections.Generic;
+
+namespace Doggo.Repositories
+{
+    public interface IWalkerRepository
+    {
+        List<Walker> GetAllWalkers();
+        Walker GetWalkerById(int id);
+    }
+}
